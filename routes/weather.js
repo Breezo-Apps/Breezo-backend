a538cdc2a087ef28d2ba6ab74646d0b21246ea4d
kiooo21 <135302345+kiooo21@users.noreply.github.com>
2024-11-24 14:19:28 +0700

    Update weather.js

--- routes/weather.js
+++ routes/weather.js
@@ -7,6 +7,26 @@ const pool = mysql.createPool({
     database: 'brezzo', 
 });
 
+// //cron job
+// async function addWeatherData() {
+//     try {
+//         const degree = Math.floor(20 + Math.random() * 15); // Suhu acak antara 20-35
+//         const weatherConditions = ['cerah', 'berawan', 'mendung'];
+//         const degreeImg = weatherConditions[Math.floor(Math.random() * weatherConditions.length)];
+
+//         const query = `INSERT INTO daily (date, degree, degree_img) VALUES (NOW(), ?, ?)`;
+//         await pool.query(query, [degree, degreeImg]);
+//         console.log('Weather data added successfully!');
+//     } catch (err) {
+//         console.error('Error adding weather data:', err);
+//     }
+// }
+
+// cron.schedule('0 * * * *', () => {
+//     console.log('Running cron job: Adding new weather data...');
+//     addWeatherData();
+// });
+
 const weatherRoutes = [{
     method: 'GET',
     path: '/api/home/daily',
